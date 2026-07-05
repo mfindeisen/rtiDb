f40f7cab15aeba94beb1175345ae0f2997762afd
Matthias Findeisen <findeisen.matthias@gmail.com>
2026-07-05 19:55:05 +0200

    chore(client): bundle modern RTI viewer v1.1.0 assets
    Update the embedded modern-rti-viewer build, refresh hashed chunk files,
    and bump the cache-buster query string in index.html.

---
+++ client/public/modern-viewer/deflate-S9orvjbB.js
@@ -0,0 +1,10 @@
+import { t as e } from "./pako.esm-Cqe9kMGq.js";
+import { t } from "./basedecoder-C39ADVdF.js";
+//#region node_modules/.pnpm/geotiff@2.1.3/node_modules/geotiff/dist-module/compression/deflate.js
+var n = class extends t {
+	decodeBlock(t) {
+		return e(new Uint8Array(t)).buffer;
+	}
+};
+//#endregion
+export { n as default };
